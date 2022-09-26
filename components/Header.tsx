7edbecb5375ca2656ad0dc70ac1e7b7c88787c3d
David Fernando <davidfernandodamata21@gmail.com>
2022-09-26 22:40:01 +0000

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,18 @@
+import styles from '../styles/Header.module.css'
+import Image from 'next/image'
+
+import logotipo from '../public/logotipo.png'
+
 function Header(){
     return (
-        <h1>Header</h1>
+        <header className={styles.header}>
+            <Image src={logotipo} alt="JJPisos" height={40} width={200}/>
+            <nav>
+                <a href="#">Início </a>
+                <a href="#">Serviços </a>
+                <a href="#">Formas de pagamento</a>
+            </nav>
+        </header>
     )
 }
 
