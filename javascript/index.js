24169aac4ac7f02efc1199f0ed6802ca19e8716d
David Fernando <davidfernandodamata21@gmail.com>
2022-10-01 16:21:32 +0000

    Add toggleActiveLink

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,17 @@
 import raiseTheButtonPosition from "./raiseTheButtonPosition.js"
+import toggleActiveLink from "./toggleActiveLink.js"
 
 const whatsappButton = document.querySelector('.whatsapp-button')
 const whatsappArea = document.querySelector('.whatsapp-area')
+const homeLink = document.querySelector('#home-link')
+const servicesLink = document.querySelector('#services-link')
+const aboutLink = document.querySelector('#about-link')
+
+const links = { 
+    homeLink, 
+    servicesLink, 
+    aboutLink 
+}
 
 const contactButtons = `<a href="https://api.whatsapp.com/send?phone=5511991645108&text=Olá Jilmar! Vim pelo site JJPisos. Gostaria de fazer um orçamento." target="_blank" class="contact">Falar com Jilmar</a>
 <a href="https://api.whatsapp.com/send?phone=5511953365061&text=Olá Jean! Vim pelo site JJPisos. Gostaria de fazer um orçamento." target="_blank" class="contact">Falar com Jean</a>`
@@ -10,7 +20,19 @@ let contactVisible = false
 
 whatsappButton.addEventListener('click', () => {
     return toggleContactsLink()
-}
+})
+
+servicesLink.addEventListener('click', () => {
+    return toggleActiveLink(links).activeServicesLink()
+})
+
+homeLink.addEventListener('click', () => {
+    return toggleActiveLink(links).activeHomeLink()
+})
+
+aboutLink.addEventListener('click', () => {
+    return toggleActiveLink(links).activeAboutLink()
+})
 
 function toggleContactsLink(){
 
